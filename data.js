0bec50367e6710652026e881428e2213019c9325
Özkan Çimenli <76163736+ozkancimenli@users.noreply.github.com>
2023-09-03 04:22:24 +0300

    data.js and some data

---
+++ data.js
@@ -0,0 +1,61 @@
+module.exports = function () {
+  return {
+    products: [
+      {
+        id: 1,
+        name: "Iphone 7",
+        price: 500,
+        imageUrl: "1.jpg",
+        description: "çok kötü",
+      },
+      {
+        id: 2,
+        name: "Iphone 8",
+        price: 600,
+        imageUrl: "2.jpg",
+        description: "kötü",
+      },
+      {
+        id: 3,
+        name: "Iphone 10",
+        price: 900,
+        imageUrl: "3.jpg",
+        description: "idare eder",
+      },
+      {
+        id: 4,
+        name: "Iphone 11",
+        price: 1000,
+        imageUrl: "4.jpg",
+        description: "normal",
+      },
+      {
+        id: 5,
+        name: "Iphone 12",
+        price: 1100,
+        imageUrl: "5.jpg",
+        description: "iyi",
+      },
+      {
+        id: 6,
+        name: "Iphone 13",
+        price: 1200,
+        imageUrl: "6.jpg",
+        description: "çok iyi",
+      },
+      {
+        id: 7,
+        name: "Iphone 14",
+        price: 1300,
+        imageUrl: "7.jpg",
+        description: "çok çok iyi",
+      }
+    ],
+    categories: [
+        {id:1, name:"Telefon"},
+        {id:2, name:"Bilgisayar"},
+        {id:3, name:"Elektronik"}
+    ],
+    orders: [],
+  };
+};
