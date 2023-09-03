aa4b8c23b2641c173f0a44967e04c43908a54364
Özkan Çimenli <76163736+ozkancimenli@users.noreply.github.com>
2023-09-03 16:37:38 +0300

    4 Repository

---
+++ src/app/shop/shop.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ModelModule } from '../model/model.module';
+
+@NgModule({
+    imports: [ModelModule],
+    providers: []
+
+})
+export class ShopModule { }
