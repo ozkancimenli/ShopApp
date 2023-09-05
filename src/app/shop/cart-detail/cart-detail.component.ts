4aee03914580adcee1d7a3ebb97aec54d3a843c7
Özkan Çimenli <76163736+ozkancimenli@users.noreply.github.com>
2023-09-06 00:25:08 +0300

    16:updating cart and all errors done

--- src/app/shop/cart-detail/cart-detail.component.ts
+++ src/app/shop/cart-detail/cart-detail.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/model/cart.model';
 
 @Component({
   selector: 'cart-detail',
   templateUrl: './cart-detail.component.html',
   styleUrls: ['./cart-detail.component.css']
 })
-export class CartDetailComponent {
+export class CartDetailComponent implements OnInit {
+  constructor(public cart: Cart) { }
 
+  ngOnInit(): void {
+
+  }
 }
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -4,13 +4,12 @@ import { CategoryRepository } from "../model/category.repository";
 import { Product } from "../model/product.model";
 import { Category } from "../model/category.model";
 import { Cart } from "../model/cart.model";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'shop',
     templateUrl: 'shop.component.html',
-    styles: [`
-    .pt-100 {padding-top:100px;}
-    `]
+  
 })
 export class ShopComponent {
     public selectedCategory: Category;
@@ -22,7 +21,8 @@ export class ShopComponent {
     constructor(
         private productRepository: ProductRepository,
         private categoryRepository: CategoryRepository,
-        private cart: Cart
+        private cart: Cart,
+        private router:Router
     ) { }
 
     get products(): Product[] {
@@ -57,5 +57,6 @@ export class ShopComponent {
 
     addProductToCart(product: Product) {
         this.cart.addItem(product);
+        this.router.navigateByUrl('/cart');
     }
 }
